cf14e739c2e8fd1f6fdfaba15987311c07bd0a7b
Brady Newhard <brady.russell@gmail.com>
2025-02-18 23:27:24 -0500

    fruit search

--- src/components/Fruits/FruitSearch/FruitSearch.jsx
+++ src/components/Fruits/FruitSearch/FruitSearch.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
 import { show } from '../../../services/fruitService'
 import FruitDetail from '../FruitDetail/FruitDetail';
+import { useNavigate } from 'react-router-dom' 
 
 function FruitSearch() {
     const [fruit, setFruit] = useState('')
     const [fruitData, setFruitData] = useState('')
+    const navigate = useNavigate()
     const handleSubmit = async (e) => {
         e.preventDefault()
         if (!fruit.trim()) return 
             try {
                 const data = await show(fruit)
-                setFruitData(data)
+                // setFruitData(data)
+                navigate('/', { state: { fruitData: data } })
             } catch (error) {
                 console.error("Error fetching fruit: ", error)
         }
@@ -33,7 +36,7 @@ function FruitSearch() {
             <button type="submit">Search</button>
             </form>
         </section>  
-        <FruitDetail fruit={fruitData} />
+        {/* <FruitDetail fruit={fruitData} /> */}
     </div>
   )
 }
